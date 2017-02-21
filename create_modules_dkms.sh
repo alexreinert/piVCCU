2be1ba14414cb365f22cf3164b61dffd22792426
Alexander Reinert <alex@areinert.de>
2017-02-21 21:22:57 +0100

    Woraround for issues in Armbian kernel header packages

--- create_modules_dkms.sh
+++ create_modules_dkms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKG_BUILD=23
+PKG_BUILD=24
 
 PKG_VERSION=1.0.$PKG_BUILD
 
--- pivccu/dkms/ensure_modules.sh
+++ pivccu/dkms/ensure_modules.sh
@@ -2,7 +2,21 @@
 modinfo generic_raw_uart &> /dev/null
 if [ ! $? -eq 0 ]; then
   PKG_VER=`dpkg -s pivccu-modules-dkms | grep '^Version: ' | cut -d' ' -f2`
-  dkms install -m pivccu -v $PKG_VER -k `uname -r`
+
+  if [ -e /usr/src/linux-headers-`uname -r` ]; then
+    cd /usr/src/linux-headers-`uname -r`
+
+    if [ `grep -c "^source \"net/wireguard/Kconfig\"" net/Kconfig` -gt 0 ]; then
+      mkdir -p net/wireguard
+      touch net/wireguard/Kconfig
+      touch net/wireguard/Makefile
+    fi
+
+    make scripts
+  fi
+
+  dkms install -m pivccu -v $PKG_VER -k `uname -r` || true
+
   for i in {1..120}; do
     udevadm settle -t 5 -E /dev/raw-uart && udevadm trigger && udevadm settle -t 5 -E /dev/raw-uart
     if [ -e /dev/raw-uart ]; then
