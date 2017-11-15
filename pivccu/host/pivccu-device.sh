13b42ad3ef6c2b5add0176e9bf24ff33ddf6cbdb
Alexander Reinert <alex@areinert.de>
2017-11-15 10:34:59 +0100

    Added parameter listavailable to pivccu-device

--- pivccu/host/pivccu-device.sh
+++ pivccu/host/pivccu-device.sh
@@ -5,9 +5,34 @@ function showUsage {
   exit 1
 }
 
-if [ $# -ne 2 ]; then
-  showUsage
-fi
+case "$1" in
+  add|delete)
+    if [ $# -ne 2 ]; then
+      showUsage
+    else
+      if [ -e $2 ]; then
+        /usr/bin/lxc-device --lxcpath /var/lib/piVCCU --name lxc $1 $2
+      else
+        echo "Device \"$2\" does not exist"
+      fi
+    fi
+    ;;
 
-/usr/bin/lxc-device --lxcpath /var/lib/piVCCU --name lxc $1 $2
+  listavailable)
+    for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+      syspath="${sysdevpath%/dev}"
+      devname="$(udevadm info -q name -p $syspath)"
+      [[ "$devname" == "bus/"* ]] && continue
+      eval "$(udevadm info -q property --export -p $syspath)"
+      [[ -z "$ID_SERIAL" ]] && continue
+      echo "/dev/$devname - $ID_SERIAL"
+    )
+    done
+    ;;
+
+  *)
+    showUsage
+    ;;
+esac
 
