e02a8cc2fe789bab4141cd1cd1a6c1f0e6ac4b5d
Alexander Reinert <alex@areinert.de>
2017-12-05 09:41:59 +0100

    Better detection if DKMS recompile needs to be done

--- pivccu/dkms/ensure_modules.sh
+++ pivccu/dkms/ensure_modules.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ ! -e /dev/raw-uart ]; then
+modinfo generic_raw_uart &> /dev/null
+if [ ! $? -eq 0 ]; then
   dpkg-reconfigure pivccu-modules-dkms
   modprobe dw_apb_raw_uart || true
   modprobe pl011_raw_uart || true
