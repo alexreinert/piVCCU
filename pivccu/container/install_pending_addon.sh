7f7ae68beaa14627ff3fb8d9906e86000d8b6e6c
Alexander Reinert <alex@areinert.de>
2017-12-07 08:21:48 +0100

    Better replacement hacks for mount commands in AddOn update_script

--- pivccu/container/install_pending_addon.sh
+++ pivccu/container/install_pending_addon.sh
@@ -12,7 +12,9 @@ if [ ! -x /var/update_script ]; then
   exit
 fi
 
-sed -i update_script -e "s/mount /: # mount/"
+sed -i update_script -e "s/mount -t ubifs ubi1:user \(.*\)/mount --bind \/usr\/local \1/"
+sed -i update_script -e "s/\(mount --bind \/usr\/local \/usr\/local.*\)/# \1/"
+sed -i update_script -e "s/\(mount \/usr\/local.*\)/# \1/"
 
 ./update_script CCU2
 
