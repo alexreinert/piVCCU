7fcd86e2eb204de719c8b08c616cb99e092e0aaf
Alexander Reinert <alex@areinert.de>
2017-12-20 17:40:42 +0100

    Fixed replacement of mount commands in addon installation scripts

--- pivccu/container/install_pending_addon.sh
+++ pivccu/container/install_pending_addon.sh
@@ -13,8 +13,8 @@ if [ ! -x /var/update_script ]; then
 fi
 
 sed -i update_script -e "s/mount -t ubifs ubi1:user \(.*\)/mount --bind \/usr\/local \1/"
-sed -i update_script -e "s/\(mount --bind \/usr\/local \/usr\/local.*\)/# \1/"
-sed -i update_script -e "s/\(mount \/usr\/local.*\)/# \1/"
+sed -i update_script -e "s/\(mount --bind \/usr\/local \/usr\/local.*\)/: # \1/"
+sed -i update_script -e "s/\(mount \/usr\/local.*\)/: # \1/"
 
 ./update_script CCU2
 
