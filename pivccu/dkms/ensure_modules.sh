22a13592551f2997f6b59c8981b4818619cfbcf3
Alexander Reinert <alex@areinert.de>
2019-01-23 19:11:02 +0100

    Build kernel modules only for active kernel when installing DKMS package

--- pivccu/dkms/ensure_modules.sh
+++ pivccu/dkms/ensure_modules.sh
@@ -2,7 +2,7 @@
 modinfo generic_raw_uart &> /dev/null
 if [ ! $? -eq 0 ]; then
   PKG_VER=`dpkg -s pivccu-modules-dkms | grep '^Version: ' | cut -d' ' -f2`
-  dkms install -m pivccu -v $PKG_VER
+  dkms install -m pivccu -v $PKG_VER -k `uname -r`
   for i in {1..120}; do
     udevadm settle -t 5 -E /dev/raw-uart && udevadm trigger && udevadm settle -t 5 -E /dev/raw-uart
     if [ -e /dev/raw-uart ]; then
