d4a972d79228af2f755ba9684ee3dd8cc38f3706
Alexander Reinert <alex@areinert.de>
2019-03-14 19:11:48 +0100

    Fixed pre-compiler switch in generic-raw-uart

--- kernel/generic_raw_uart.c
+++ kernel/generic_raw_uart.c
@@ -719,7 +719,7 @@ int generic_raw_uart_get_gpio_pin_number(struct generic_raw_uart_instance *insta
   {
     struct fwnode_handle *fwnode = dev_fwnode(dev);
     const char *label = generic_raw_uart_get_pin_label(pin);
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,11,0))
     struct gpio_desc *gpiod = fwnode_get_named_gpiod(fwnode, label, 0, GPIOD_ASIS, label);
 #else
     struct gpio_desc *gpiod = fwnode_get_named_gpiod(fwnode, label);
@@ -950,7 +950,7 @@ module_exit(generic_raw_uart_exit);
 
 MODULE_ALIAS("platform:generic-raw-uart");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.8");
+MODULE_VERSION("1.9");
 MODULE_DESCRIPTION("generic raw uart driver for communication of piVCCU with the HM-MOD-RPI-PCB and RPI-RF-MOD radio modules");
 MODULE_AUTHOR("Alexander Reinert <alex@areinert.de>");
 
