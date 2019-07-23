9380c36e748bbc9e7ac4c060eb0af08ecc2463e7
Alexander Reinert <alex@areinert.de>
2019-07-23 11:18:44 +0200

    Update to CCU3 firmware 3.47.15

--- create_pivccu3.sh
+++ create_pivccu3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CCU_VERSION=3.47.10
-CCU_DOWNLOAD_SPLASH_URL="http://www.eq-3.de/service/downloads.html?id=315"
+CCU_VERSION=3.47.15
+CCU_DOWNLOAD_SPLASH_URL="http://www.eq-3.de/service/downloads.html?id=320"
 CCU_DOWNLOAD_URL="https://www.eq-3.de/Downloads/Software/CCU3-Firmware/CCU3-$CCU_VERSION/ccu3-$CCU_VERSION.tgz"
 
-PKG_BUILD=29
+PKG_BUILD=30
 
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
