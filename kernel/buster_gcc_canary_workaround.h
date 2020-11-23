4c694224d5dd14e498082561be1e8219c13e52db
Thomas Dallmair <dallmair@users.noreply.github.com>
2020-11-23 11:21:44 +0100

    Read canary from running kernel. Dup message of __stack_chk_fail from mainline kernel.

--- kernel/buster_gcc_canary_workaround.h
+++ kernel/buster_gcc_canary_workaround.h
@@ -4,13 +4,12 @@ unsigned long __stack_chk_guard;
 
 void __stack_chk_guard_setup(void)
 {
-  __stack_chk_guard = 0xB233A44D;
+  __stack_chk_guard = current->stack_canary;
 }
 
 void __stack_chk_fail(void)
 {
-  dump_stack ();
-  panic ("Stack-protector:kernel stack is corrupted in:%pa\n",
+  panic ("stack-protector: Kernel stack is corrupted in: %pB",
     __builtin_return_address (0));
 }
 
