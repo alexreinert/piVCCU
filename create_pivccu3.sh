8b99edbe9c43afdd834cd989e2d89ea6bc74bf83
Alexander Reinert <alex@areinert.de>
2021-06-29 16:38:10 +0200

    Updated to CCU3 firmware 3.59.6

--- create_pivccu3.sh
+++ create_pivccu3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CCU_VERSION=3.57.5
+CCU_VERSION=3.59.6
 CCU_DOWNLOAD_SPLASH_URL="https://www.eq-3.de/service/downloads.html"
 CCU_DOWNLOAD_URL="https://www.eq-3.de/downloads/software/firmware/ccu3-firmware/ccu3-$CCU_VERSION.tgz"
 
-PKG_BUILD=57
+PKG_BUILD=58
 
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
@@ -81,8 +81,8 @@ dpkg-deb --build pivccu3-$PKG_VERSION
 
 cp pivccu3-$PKG_VERSION.deb $CURRENT_DIR/pivccu3-$PKG_VERSION-armhf.deb
 
-wget -O openjdk-8-jre.deb http://security.debian.org/debian-security/pool/updates/main/o/openjdk-8/openjdk-8-jre_8u275-b01-1~deb9u1_armhf.deb
-wget -O openjdk-8-jre-headless.deb http://security.debian.org/debian-security/pool/updates/main/o/openjdk-8/openjdk-8-jre-headless_8u275-b01-1~deb9u1_armhf.deb
+wget -O openjdk-8-jre.deb http://security.debian.org/debian-security/pool/updates/main/o/openjdk-8/openjdk-8-jre_8u292-b10-0+deb9u1_arm64.deb
+wget -O openjdk-8-jre-headless.deb http://security.debian.org/debian-security/pool/updates/main/o/openjdk-8/openjdk-8-jre-headless_8u292-b10-0+deb9u1_armhf.deb
 
 dpkg-deb -x openjdk-8-jre.deb .
 dpkg-deb -x openjdk-8-jre-headless.deb .
