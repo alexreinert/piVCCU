eb6dba369a24cd8f5fdf2032f3f92764fe77bd38
Alexander Reinert <alex@areinert.de>
2023-04-04 18:30:21 +0200

    Updated to CCU3 firmware 3.69.6

--- create_pivccu3.sh
+++ create_pivccu3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CCU_VERSION=3.67.10
+CCU_VERSION=3.69.6
 CCU_DOWNLOAD_SPLASH_URL="https://www.eq-3.de/service/downloads.html"
 CCU_DOWNLOAD_URL="https://www.eq-3.de/downloads/software/firmware/ccu3-firmware/ccu3-$CCU_VERSION.tgz"
 CCU_DOWNLOAD_URL="https://homematic-ip.com/sites/default/files/downloads/ccu3-$CCU_VERSION.tgz"
 
-PKG_BUILD=78
+PKG_BUILD=79
 
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
