f82040585ca9da57106a444f881e44a4ca397ff3
Alexander Reinert <alex@areinert.de>
2023-04-21 16:57:30 +0200

    - Add automatic fix for mismatch of architecture of kernel and binaries on Raspberry Pi - Optimized terminal output on deb package scripts for package pivccu-modules-dkms

--- create_modules_dkms.sh
+++ create_modules_dkms.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-PKG_BUILD=80
+PKG_BUILD=81
 
 PKG_VERSION=1.0.$PKG_BUILD
 
+function throw {
+  echo $1
+  exit 1
+}
+
+function run {
+  echo -n "$1 ... "
+  shift
+  ERR=`$* 2>&1` && RC=$? || RC=$?
+  if [ $RC -eq 0 ]; then
+    echo -e "\033[0;32mDone\033[0;0m"
+  else
+    echo -e "\033[1;91mFAILED\033[0;0m"
+    echo "$ERR"
+    exit 1
+  fi
+}
+
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
-
-cd $WORK_DIR
-
 TARGET_DIR=$WORK_DIR/pivccu-modules-dkms-$PKG_VERSION
 
-mkdir -p $TARGET_DIR/usr/src/pivccu-$PKG_VERSION
-cp $CURRENT_DIR/kernel/* $TARGET_DIR/usr/src/pivccu-$PKG_VERSION
+function create_package_dir {
+  mkdir -p $TARGET_DIR/usr/src/pivccu-$PKG_VERSION || throw "Could not create temporary directory"
+}
 
-DKMS_CONF_FILE=$TARGET_DIR/usr/src/pivccu-$PKG_VERSION/dkms.conf
+function create_dkms_config {
+  DKMS_CONF_FILE=$TARGET_DIR/usr/src/pivccu-$PKG_VERSION/dkms.conf
 
-cat <<EOF >> $DKMS_CONF_FILE
+  cat <<EOF >> $DKMS_CONF_FILE
 PACKAGE_NAME="pivccu"
 PACKAGE_VERSION="$PKG_VERSION"
 
@@ -26,36 +43,47 @@ AUTOINSTALL="yes"
 
 EOF
 
-index=0
-for file in $TARGET_DIR/usr/src/pivccu-$PKG_VERSION/*.c; do
-  modname=$(basename "$file" .c)
-  echo "BUILT_MODULE_NAME[$index]=\"$modname\"" >> $DKMS_CONF_FILE
-  echo "DEST_MODULE_LOCATION[$index]=\"/kernel/drivers/pivccu\"" >> $DKMS_CONF_FILE
-  index=$(expr $index + 1)
-done
+  index=0
+  for file in $TARGET_DIR/usr/src/pivccu-$PKG_VERSION/*.c; do
+    modname=$(basename "$file" .c)
+    echo "BUILT_MODULE_NAME[$index]=\"$modname\"" >> $DKMS_CONF_FILE
+    echo "DEST_MODULE_LOCATION[$index]=\"/kernel/drivers/pivccu\"" >> $DKMS_CONF_FILE
+    index=$(expr $index + 1)
+  done
+}
 
-mkdir -p $TARGET_DIR/lib/systemd/system/
-cp -p $CURRENT_DIR/pivccu/dkms/*.service $TARGET_DIR/lib/systemd/system/
-cp -p $CURRENT_DIR/pivccu/rtc/*.service $TARGET_DIR/lib/systemd/system/
-cp -p $CURRENT_DIR/pivccu/rtc/*.timer $TARGET_DIR/lib/systemd/system/
+function copy_files {
+  cp $CURRENT_DIR/kernel/* $TARGET_DIR/usr/src/pivccu-$PKG_VERSION
+  mkdir -p $TARGET_DIR/lib/systemd/system/
+  cp -p $CURRENT_DIR/pivccu/dkms/*.service $TARGET_DIR/lib/systemd/system/
+  cp -p $CURRENT_DIR/pivccu/rtc/*.service $TARGET_DIR/lib/systemd/system/
+  cp -p $CURRENT_DIR/pivccu/rtc/*.timer $TARGET_DIR/lib/systemd/system/
 
-mkdir -p $TARGET_DIR/var/lib/piVCCU/dkms
-cp -p $CURRENT_DIR/pivccu/dkms/*.sh $TARGET_DIR/var/lib/piVCCU/dkms
+  mkdir -p $TARGET_DIR/var/lib/piVCCU/dkms
+  cp -p $CURRENT_DIR/pivccu/dkms/*.sh $TARGET_DIR/var/lib/piVCCU/dkms
 
-mkdir -p $TARGET_DIR/lib/udev/rules.d
-cp -p $CURRENT_DIR/pivccu/rtc/*.rules $TARGET_DIR/lib/udev/rules.d
+  mkdir -p $TARGET_DIR/lib/udev/rules.d
+  cp -p $CURRENT_DIR/pivccu/rtc/*.rules $TARGET_DIR/lib/udev/rules.d
+}
 
-mkdir -p $TARGET_DIR/DEBIAN
-cp -p $CURRENT_DIR/package/pivccu-modules-dkms/* $TARGET_DIR/DEBIAN
-for file in $TARGET_DIR/DEBIAN/*; do
-  sed -i "s/{PKG_VERSION}/$PKG_VERSION/g" $file
-done
+function create_package_files {
+  mkdir -p $TARGET_DIR/DEBIAN
+  cp -p $CURRENT_DIR/package/pivccu-modules-dkms/* $TARGET_DIR/DEBIAN
+  for file in $TARGET_DIR/DEBIAN/*; do
+    sed -i "s/{PKG_VERSION}/$PKG_VERSION/g" $file
+  done
+}
 
 cd $WORK_DIR
 
-dpkg-deb --build -Zxz pivccu-modules-dkms-$PKG_VERSION
+run "Create package directory" create_package_dir
+run "Copy files" copy_files
+run "Create DKMS config file" create_dkms_config
+run "Create package meta files" create_package_files
 
-cp pivccu-modules-dkms-*.deb $CURRENT_DIR
+cd $WORK_DIR
 
-echo "Please clean-up the work dir temp folder $WORK_DIR, e.g. by doing rm -R $WORK_DIR"
+run "Create deb package" dpkg-deb --build -Zxz pivccu-modules-dkms-$PKG_VERSION
+run "Copy package to local directory" cp pivccu-modules-dkms-*.deb $CURRENT_DIR
+run "Cleanup temporary files" rm -rf $WORK_DIR
 
--- pivccu/dkms/ensure_modules.sh
+++ pivccu/dkms/ensure_modules.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
-modinfo generic_raw_uart &> /dev/null && RC=$? || RC=$?
-if [ ! $RC -eq 0 ]; then
-  PKG_VER=`dpkg -s pivccu-modules-dkms | grep '^Version: ' | cut -d' ' -f2`
+function throw {
+  echo $1
+  exit 1
+}
 
-  dkms remove -m pivccu -v $PKG_VER -k `uname -r` || true
+function run {
+  echo -n "$1 ... "
+  shift
+  ERR=`$* 2>&1` && RC=$? || RC=$?
+  if [ $RC -eq 0 ]; then
+    echo -e "\033[0;32mDone\033[0;0m"
+  else
+    echo -e "\033[1;91mFAILED\033[0;0m"
+    echo "$ERR"
+    exit 1
+  fi
+}
 
-  if [ -e /usr/src/linux-headers-`uname -r` ]; then
-    cd /usr/src/linux-headers-`uname -r`
+KERNEL_VER=`uname -r`
 
-    if [ -e scripts/basic/fixdep ]; then
-      scripts/basic/fixdep >/dev/null 2>&1 && RC=$? || RC=$?
-      if [ $RC -eq 126 ]; then
-        make scripts || true
-      fi
+function check_for_headers {
+  if [ ! -e /usr/src/linux-headers-$KERNEL_VER ]; then
+    throw "No headers found for current active kernel $KERNEL_VER"
+  fi
+}
+
+function prepare_headers {
+  cd /usr/src/linux-headers-$KERNEL_VER
+  if [ ! -x scripts/recordmcount ]; then
+    if [ `grep -c "^source \"net/wireguard/Kconfig\"" net/Kconfig` -gt 0 ]; then
+      mkdir -p net/wireguard
+      touch net/wireguard/Kconfig
+      touch net/wireguard/Makefile
     fi
+
+    make scripts
 
     if [ ! -x scripts/recordmcount ]; then
-      if [ `grep -c "^source \"net/wireguard/Kconfig\"" net/Kconfig` -gt 0 ]; then
-        mkdir -p net/wireguard
-        touch net/wireguard/Kconfig
-        touch net/wireguard/Makefile
-      fi
-
-      make scripts
-
-      if [ ! -x scripts/recordmcount ]; then
-        make EXTRAVERSION="-`uname -r | cut -d- -f2-`" prepare0
-	cd /usr/src/linux-headers-`uname -r`/scripts
-	make recordmcount
-      fi
+      make EXTRAVERSION="-`uname -r | cut -d- -f2-`" prepare0
+      cd /usr/src/linux-headers-$KERNEL_VER/scripts
+      make recordmcount
     fi
   fi
+}
 
-  dkms install -m pivccu -v $PKG_VER -k `uname -r` || true
-
+function reload_udev {
   modinfo generic_raw_uart &> /dev/null && RC=$? || RC=$?
   if [ $RC -eq 0 ]; then
     if [ -e /etc/default/hb_rf_eth ]; then
@@ -51,5 +62,21 @@ if [ ! $RC -eq 0 ]; then
       done
     fi
   fi
+}
+
+modinfo generic_raw_uart &> /dev/null && RC=$? || RC=$?
+if [ ! $RC -eq 0 ]; then
+  PKG_VER=`dpkg -s pivccu-modules-dkms | grep '^Version: ' | cut -d' ' -f2`
+
+  DKMS_STATUS=`dkms status -m pivccu -v $PKG_VER -k $KERNEL_VER`
+  if [ ! -z "$DKMS_STATUS" ]; then
+    run "Remove DKMS package" dkms remove -m pivccu -v $PKG_VER -k `uname -r`
+  fi
+
+  run "Check kernel headers" check_for_headers
+
+  run "Prepare kernel headers" prepare_headers
+
+  run "Install DKMS package" dkms install -m pivccu -v $PKG_VER -k $KERNEL_VER
 fi
 
